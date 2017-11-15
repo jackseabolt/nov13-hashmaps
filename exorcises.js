8b9620a7bdafcea352686c412d4d145cc0bb81d3
matthew lee <matthewlee07@gmail.com>
2017-11-15 00:56:00 -0500

    removed linkedlist, copied palindrome

--- exorcises.js
+++ exorcises.js
@@ -1,26 +1,46 @@
 const { HashMap } = require('./index');
 
-function isPalindrome(string) {
-    let myHash = new HashMap();
-
-    for (let i = 0; i < string.length; i++) {
-        myHash.set(string[i], 1)
-    }
-    let oddCounter = 0;
-
-    for (let i = 0; i < string.length; i++) {
-        let currentLetterList = myHash.get(string[i])
-        console.log(myHash.get(string[i]));
-        if (currentLetterList.length % 2 !== 0) {
-            oddCounter++
-            
+function coantainsKey(hTable, key){
+    for(let i=0; i<hTable.hashTable.length; i++){
+        if(hTable.get(key)){
+            return true;
         }
     }
-    if (oddCounter > 1) {
-        return false
-    }
-    return true;
+    return false;
 }
 
-console.log(isPalindrome('racecar'));
-//aabbaac
+function PermutationString(string){
+    let oddChar = false;
+    let hm = new HashMap();
+    for(let i=0;i<string.length;i++){
+        if(coantainsKey(hm,string[i])){
+            let value = hm.get(string[i])
+            hm.set(string[i],(value+1))
+        }
+       else{
+            hm.set(string[i],1);
+       }
+    }
+     for (let i = 0; i < hm.hashTable.length; i++){ 
+       for (let keys in hm.hashTable[i]){
+            if (hm.hashTable[i][keys] & 1){
+                if (oddChar){
+                    return false;
+                } 
+	    oddChar = true;
+            }
+        }  
+     }
+        
+    //  displayHashMapKeys(hm);
+     return true;;
+}
+
+function mainPalindromString(){
+    //console.log(PermutationString('madam'));
+    console.log('cllic: '+PermutationString('cllic'));
+    console.log('aaxxis '+PermutationString('aaxxis'));
+    console.log('caabl '+PermutationString('caabl'));
+}
+
+mainPalindromString();
--- index.js
+++ index.js
@@ -1,132 +1,132 @@
-const { LinkedList } = require('./linkedlist');
-
-
 class HashMap {
-    constructor(initialCapacity = 8) {
+    constructor(initialCapacity=8) {
         this.length = 0;
-        this._slots = [];
+        this.hashTable = [];
         this._capacity = initialCapacity;
         this._deleted = 0;
     }
-
-    get(key) {
-        const index = this._findSlot(key);
-        
-        if (this._slots[index] === undefined) {
-            throw new Error('Key error');
+    _findSlot(key) {
+        const hash = HashMap._hashString(key);
+        const index = hash % this._capacity;
+        let slot = this.hashTable[index];
+        if (!slot) {
+            return this.hashTable[index] = {key};
         }
-        return this._slots[index].get(key);
+        if (slot.key == key) 
+            return slot;
+        while (slot.next) {
+            slot = slot.next;
+            if (slot.key == key) 
+                return slot;
+        }
+        return slot.next = {key};
     }
 
-    set(key, value) {
-        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-        // if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-        //     this._resize(this._capacity * HashMap.SIZE_RATIO);
-        // }
-
-        const index = this._findSlot(key);
-        
-        if (!this._slots[index]) {
-            const hashLink = new LinkedList();
-            this.length++;
-            this._slots[index] = hashLink;
-            hashLink.insert(0, value, key);
+    static _hashString(string) {
+        let hash = 5381;
+        for (let i=0; i<string.length; i++) {
+        //Bitwise left shift with 5 0s - this would be similar to
+        //hash*31, 31 being the decent prime number
+        //but bit shifting is a faster way to do this
+        //tradeoff is understandability
+            hash = (hash << 5) + hash + string.charCodeAt(i);
+            //converting hash to a 32 bit integer
+            hash = hash & hash;
         }
-        else {
-            let current = index.head;
-            for (let i=0; i<index.length; i++){
-                if (current.key===key){
-                    return current.value = value
-                } 
-                current = current.next
+        //making sure has is unsigned - meaning non-negtive number
+        return hash >>> 0;
+    }
+     _resize(size) {
+        const oldSlots = this.hashTable;
+        this._capacity = size;
+        // Reset the length - it will get rebuilt as you add the items back
+        this.length = 0;
+        this._deleted = 0;
+        this.hashTable = [];
+
+        for (let i=0; i<oldSlots.length; i++)
+            for (let slot = oldSlots[i]; slot; slot = slot.next) {
+                if (!slot.deleted)
+                    //copy to a new array
+                    this.set(slot.key, slot.value);
             }
-            const indexList = this._slots[index];
-            indexList.insert(0, value, key)
-
-        }
+                
     }
 
-    remove(key) {
-        const index = this._findSlot(key);
-        const slot = this._slots[index];
-        if (slot === undefined) {
-            throw new Error('Key error');
+    set(key, value){
+        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * HashMap.SIZE_RATIO);
         }
+        const index = this._findSlot(key);
+        if (index.deleted !== false) 
+            this.length++;
+        index.value = value;
+        index.deleted = false;
+    }
+
+    remove(key){
+        const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        }
+        const slot = this._findSlot(key);
+        if (slot.deleted) 
+            throw new Error('Key error');
         slot.deleted = true;
         this.length--;
         this._deleted++;
     }
-
-    _findSlot(key) {
-        const hash = HashMap._hashString(key);
-        const index = hash % this._capacity;
-        return index;
-
-        // for (let i=start; i<start + this._capacity; i++) {
-        //     const index = i % this._capacity;
-        //     const slot = this._slots[index];
-        //     if (slot === undefined || (slot.key == key && !slot.deleted)) {
-        //         return index;
-        //     }
-        // }
+  
+    get(key) {
+        const slot = this._findSlot(key);
+        if (slot.deleted) 
+            throw new Error('Key error');
+        return slot.value;
     }
-
-    // _resize(size) {
-    //     const oldSlots = this._slots;
-    //     this._capacity = size;
-    //     // Reset the length - it will get rebuilt as you add the items back
-    //     this.length = 0;
-    //     this._deleted = 0;
-    //     this._slots = [];
-
-    //     for (const slot of oldSlots) {
-    //         if (slot !== undefined && !slot.deleted) {
-    //             this.set(slot.key, slot.value);
-    //         }
-    //     }
-    // }
-
-    static _hashString(string) {
-        let hash = 5381;
-        for (let i = 0; i < string.length; i++) {
-            hash = (hash << 5) + hash + string.charCodeAt(i);
-            hash = hash & hash;
-        }
-        return hash >>> 0;
+    //Enumerate all valid keys in the map and save them to an array
+    //this way you don't have to access the HashMap members
+    keys() {
+        const ret = [];
+        for (let i = 0; i < this.hashTable.length; ++i)
+            for (let slot = this.hashTable[i]; slot; slot = slot.next)
+                if (!slot.deleted)
+                    ret.push(slot.key);
+        return ret;
     }
 }
+
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
 
 // ----------------------------------------- //
-
-// let myHash = new HashMap();
-// myHash.set("Hobbit", "Bilbo")
+/*
+let myHash = new HashMap();
+myHash.set("Hobbit", "Bilbo")
 // console.log(myHash.get("Hobbit"))
-// myHash.set("Wizard", "Gandolf")
+myHash.set("Wizard", "Gandolf")
 // console.log(myHash.get("Wizard"))
-// myHash.set("Hobbit", "Frodo")
+myHash.set("Hobbit", "Frodo")
 // console.log(myHash.get("Hobbit"))
-// myHash.set("Human", "Aragon")
+myHash.set("Human", "Aragon")
 // console.log(myHash.get("Human"))
-// myHash.set("Elf", "Legolas")
+myHash.set("Elf", "Legolas")
 // console.log(myHash.get("Elf"))
-// myHash.set("Maiar", "The Necromancer")
+myHash.set("Maiar", "The Necromancer")
 // console.log(myHash.get("Maiar"))
-// myHash.set("RingBearer", "Gollum")
+myHash.set("RingBearer", "Gollum")
 // console.log(myHash.get("RingBearer"))
-// myHash.set("LadyOfLight", "Galadriel")
+myHash.set("LadyOfLight", "Galadriel")
 // console.log(myHash.get("LadyOfLight"))
-// myHash.set("HalfElven", "Arwen")
+myHash.set("HalfElven", "Arwen")
 // console.log(myHash.get("HalfElven"))
-// myHash.set("Ent", "Treebeard")
+myHash.set("Ent", "Treebeard")
 // console.log(myHash.get("Ent"))
-// myHash.set("Maiar", "Sauron")
-// console.log(myHash.get("Maiar"))
+myHash.set("Maiar", "Sauron")
+console.log(myHash.get("Maiar"))
 
-// console.log('length:', myHash.length)
-// let myList = new LinkedList; 
-
+console.log('length:', myHash.length)
+*/
 exports.HashMap = HashMap;
